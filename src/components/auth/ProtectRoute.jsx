6bc10aaccf31b35113e3a312f5b08808cb1924e5
SaunakNandi <saunaknandi021000@gmail.com>
2025-03-09 15:58:40 +0530

    unnecessary props removed

--- src/components/auth/ProtectRoute.jsx
+++ src/components/auth/ProtectRoute.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Navigate,Outlet } from 'react-router-dom'
 
-const ProtectRoute = ({children,user,redirect='/login',txt="xd"}) => {
+const ProtectRoute = ({children,user,redirect='/login'}) => {
   console.log(user)
   if (!user) return <Navigate to={redirect} />;
   if(children)
   {
-    console.log("Hello")
+    // console.log("Hello")
     return children
   }
   else{
-    console.log("Brooo");
+    // console.log("Brooo");
     return <Outlet />
   }
 } 
